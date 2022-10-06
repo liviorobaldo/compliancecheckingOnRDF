5fd39c6ae37a890156689eb2e3fd4b10194a5572
liviorobaldo <40432737+liviorobaldo@users.noreply.github.com>
2022-10-06 09:55:52 +0100

    Add files via upload

---
+++ SHACL/SHACL_fuseki/runSparqlEndpointClient.bat
@@ -0,0 +1,21 @@
+REM You need to run the files below one by one, in parallel with the same name of the OWL file uploaded on fuseki server.
+
+REM java -cp .;./lib/* -Dfile.encoding=utf-8 fusekiSparqlEndpointClient "licenceusecaseTBox.owl" "riolOntology.owl" "regulativerules.ttl" "compliancerules.ttl" "rdfABox_Size50_Probability50.owl" >> "../../evaluationSHACLonSparqlEndpoint.txt"
+
+REM java -cp .;./lib/* -Dfile.encoding=utf-8 fusekiSparqlEndpointClient "licenceusecaseTBox.owl" "riolOntology.owl" "regulativerules.ttl" "compliancerules.ttl" "rdfABox_Size100_Probability50.owl" >> "../../evaluationSHACLonSparqlEndpoint.txt"
+
+REM java -cp .;./lib/* -Dfile.encoding=utf-8 fusekiSparqlEndpointClient "licenceusecaseTBox.owl" "riolOntology.owl" "regulativerules.ttl" "compliancerules.ttl" "rdfABox_Size150_Probability50.owl" >> "../../evaluationSHACLonSparqlEndpoint.txt"
+
+REM java -cp .;./lib/* -Dfile.encoding=utf-8 fusekiSparqlEndpointClient "licenceusecaseTBox.owl" "riolOntology.owl" "regulativerules.ttl" "compliancerules.ttl" "rdfABox_Size200_Probability50.owl" >> "../../evaluationSHACLonSparqlEndpoint.txt"
+
+REM java -cp .;./lib/* -Dfile.encoding=utf-8 fusekiSparqlEndpointClient "licenceusecaseTBox.owl" "riolOntology.owl" "regulativerules.ttl" "compliancerules.ttl" "rdfABox_Size250_Probability50.owl" >> "../../evaluationSHACLonSparqlEndpoint.txt"
+
+REM java -cp .;./lib/* -Dfile.encoding=utf-8 fusekiSparqlEndpointClient "licenceusecaseTBox.owl" "riolOntology.owl" "regulativerules.ttl" "compliancerules.ttl" "rdfABox_Size300_Probability50.owl" >> "../../evaluationSHACLonSparqlEndpoint.txt"
+
+REM java -cp .;./lib/* -Dfile.encoding=utf-8 fusekiSparqlEndpointClient "licenceusecaseTBox.owl" "riolOntology.owl" "regulativerules.ttl" "compliancerules.ttl" "rdfABox_Size350_Probability50.owl" >> "../../evaluationSHACLonSparqlEndpoint.txt"
+
+REM java -cp .;./lib/* -Dfile.encoding=utf-8 fusekiSparqlEndpointClient "licenceusecaseTBox.owl" "riolOntology.owl" "regulativerules.ttl" "compliancerules.ttl" "rdfABox_Size400_Probability50.owl" >> "../../evaluationSHACLonSparqlEndpoint.txt"
+
+REM java -cp .;./lib/* -Dfile.encoding=utf-8 fusekiSparqlEndpointClient "licenceusecaseTBox.owl" "riolOntology.owl" "regulativerules.ttl" "compliancerules.ttl" "rdfABox_Size450_Probability50.owl" >> "../../evaluationSHACLonSparqlEndpoint.txt"
+
+java -cp .;./lib/* -Dfile.encoding=utf-8 fusekiSparqlEndpointClient "licenceusecaseTBox.owl" "riolOntology.owl" "regulativerules.ttl" "compliancerules.ttl" "rdfABox_Size500_Probability50.owl" >> "../../evaluationSHACLonSparqlEndpoint.txt"
